0aa970701519c54df6ff9b2cde09cd63f71b8a1e
marahjamal123 <marahbanisalameh9@gmail.com>
2026-03-16 01:07:56 +0300

    Add files via upload

---
+++ lib/classes/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class AppColors  {
+  static const ScaffoldBackgroundColor=Color.fromARGB(255, 252, 245, 222);
+  static const btnColor=Color.fromARGB(255, 250, 233, 190);
+  static const textcolor=Color.fromARGB(255, 61, 24, 5);
+  static const bordercolor=Color.fromARGB(255, 61, 24, 5);
+  static const star=Color.fromRGBO(255, 255, 193, 7);
+  static const txt2col=Color.fromARGB(255,107,60,35);
+  }
